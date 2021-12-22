cdbd71577d8ecde7b6672ec54bd8967e4ea6887e
Andrea Rastelli <rastelliandrea87@gmail.com>
2021-12-22 10:59:53 +0100

    Day 13 - Solution to part 1 and 2

--- src/AOC2021/day_13/p1.py
+++ src/AOC2021/day_13/p1.py
@@ -1,44 +1,69 @@
 from pathlib import Path
 
 
-input_file = Path(__file__).parent / Path("p1_input")
+def folding(coordinate_list, folding_instructions):
+    new_coords = set()
 
-coordinates, folding_instructions = input_file.open().read().split("\n\n")
+    for coordinate in coordinate_list:
+        x, y = coordinate
+        x1, y1 = x, y
 
-coordinates = [
-    tuple(map(int, coordinate.split(",")))
-    for coordinate in coordinates.split("\n")
-]
-# print(coordinates)
+        for fold_instruction in folding_instructions:
+            direction, amount = fold_instruction
 
-folding_instructions = [
-    instruction.strip().split(" ")[-1].split("=")
-    for instruction in folding_instructions.split("\n")
-]
-folding_instructions = [
-    (direction, int(position))
-    for direction, position in folding_instructions
-]
-# print(folding_instructions)
+            if direction == "x" and x1 > amount:
+                x1 = x1 - (2 * (x1 - amount))
 
-max_x = max(x for x, y in coordinates)
-max_y = max(y for x, y in coordinates)
+            elif direction == "y" and y1 > amount:
+                y1 = y1 - (2 * (y1 - amount))
+
+        new_coords.add((x1, y1))
+
+    return new_coords
 
 
-first_instruction = folding_instructions[0]
+if __name__ == "__main__":
+    input_file = Path(__file__).parent / Path("p1_input")
 
-new_coordinates = []
-for x, y in sorted(coordinates):
-    print(f"({x}, {y}) / ({x - first_instruction[1] if x > first_instruction[1] else x}, {y})")
-    if x > first_instruction[1]:
-        new_coordinates.append((x-first_instruction[1], y))
-    else:
-        new_coordinates.append((x, y))
+    coordinates, folding_instructions = input_file.open().read().split("\n\n")
 
-print(first_instruction)
-print(len(set(new_coordinates)))
+    coordinates = [
+        tuple(map(int, coordinate.split(",")))
+        for coordinate in coordinates.split("\n")
+    ]
 
-# print(coordinates)
-# print(len(set(coordinates)))
+    folding_instructions = [
+        instruction.strip().split(" ")[-1].split("=")
+        for instruction in folding_instructions.split("\n")
+    ]
+    folding_instructions = [
+        (direction, int(position))
+        for direction, position in folding_instructions
+    ]
+
+    first_fold = [folding_instructions[0]]
+    new_coords = folding(coordinates, first_fold)
+    print(f"Points after just the first fold: {len(new_coords)}")
+
+    new_coords = folding(coordinates, folding_instructions)
+    print(f"Points after all the folding: {len(new_coords)}")
+
+    # Prints the letters on screen
+    # Retrieve the max X and Y coordinates in order to better show the graphics
+    # on the terminal
+    max_x = max(x for x, y in new_coords)
+    max_y = max(y for x, y in new_coords)
+
+    print(f"Code in a {max_x+1}x{max_y+1} grid.")
+    for i in range(max_y+1):
+        for j in range(max_x+1):
+            if (j, i) in new_coords:
+                print("#", end="")
+            else:
+                print(" ", end="")
+
+        print("")
+
+
 
 
